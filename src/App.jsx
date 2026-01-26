7a510b75224883f8393e4e4cf890ca78e7491d45
Melanie <git@melanie-busse.de>
2026-01-26 17:14:29 +0100

    A1: create one color-card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
-import "./App.css";
+import "./Components/Color/Color.css";
 
 function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+    return (
+        <div className="colors-grid">  {/* Fragment – direkt in #root */}
+            <Color
+                color="#ff4a11"
+                role="primary main"
+                contrastText="#FFFFFF" />
+        </div>
+    );
 }
-
 export default App;
