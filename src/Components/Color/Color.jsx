5b8514111468d692227b32eb6303a73c48c81db2
Melanie <git@melanie-busse.de>
2026-01-27 12:59:36 +0100

    A4: insert edit button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,4 +1,4 @@
-export default function Color({ color, role, contrastText, id, onDelete }) {
+export default function Color({ color, role, contrastText, id, onDelete, onEdit }) {
     const borderStyle = `4px solid ${color}`;
 
   return (
@@ -14,11 +14,18 @@ export default function Color({ color, role, contrastText, id, onDelete }) {
           <div>{role|| 'primary'}</div>
           <div>contrast: {contrastText || 'white'}</div>
           <button
+              className="edit-btn"
+              onClick={() => onEdit(id)}
+              aria-label="Farbe ändern"
+          >
+              🗑️
+          </button>
+          <button
               className="delete-btn"
               onClick={() => onDelete(id)}
               aria-label="Farbe löschen"
           >
-              🗑️
+              ✎
           </button>
       </div>
   );
